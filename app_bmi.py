7ac017e2ec97f18d23fc2fdeefb43b1b510b681b
Mateusz <kochanski.mateusz94@gmail.com>
2020-12-07 15:41:21 +0100

    gitignore + requirements

--- app_bmi.py
+++ app_bmi.py
@@ -1,25 +1,28 @@
+"""Kalkulator BMI"""
 from flask import Flask, request
 from flask import render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
-def bmi():
-    if request.method =='POST' and 'weight' in request.form and 'height' in request.form and request.form.get('weight') != '' and request.form.get('height') != '':
+def bmi_app():
+    """Funkcja realizująca kalkulator"""
+    if request.method =='POST' and 'weight' in request.form and 'height' in request.form and\
+            request.form.get('weight') != '' and request.form.get('height') != '':
         w = float(request.form.get('weight'))
         h = float(request.form.get('height'))
         if h>0 and w>0:
             bmi = w/((h/100)**2)
-            if (bmi < 17):
+            if bmi < 17:
                 text = "Wychudzenie!"
-            elif (bmi >= 17 and bmi < 18.5):
+            elif bmi >= 17 and bmi < 18.5:
                 text = "Niedowaga!"
-            elif (bmi >= 18.5 and bmi < 25):
+            elif bmi >= 18.5 and bmi < 25:
                 text = "Waga prawidłowa!"
-            elif (bmi >= 25 and bmi < 30):
+            elif bmi >= 25 and bmi < 30:
                 text = "Nadwaga!"
-            elif (bmi >= 30 and bmi < 40):
+            elif bmi >= 30 and bmi < 40:
                 text = "Otyłość!"
-            elif (bmi >= 40):
+            elif bmi >= 40:
                 text = "Skrajna otyłość!"
             return render_template('bmi_form.html', bmi=bmi, w=w, h=h, text=text)
         else:
